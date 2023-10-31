dd749af0568f7d29b1a4e5ffc7e3e90d165c9376
Akintobi Moyinoluwa Ezekiel <akintobi41moyinoluwa@gmail.com>
2023-10-31 21:55:39 +0100

    final changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -60,17 +60,19 @@ const Home = ({ toggle, setToggle }) => {
   if (refresh) {
     localStorage.setItem("data", JSON.stringify(data));
   }
-
   function fetchData() {
     setData([]);
     isLoading(false);
     fetch(allTasks, options)
       .then((res) => {
         // Check if the response is OK; otherwise, throw an error
-        if (!res.ok)
+        if (!res.ok) {
+          console.log(res);
+
           throw Error(
             "No tasks available at the moment. Please try again later...",
           );
+        }
         return res.json();
       })
       .then((res) => {
@@ -83,6 +85,7 @@ const Home = ({ toggle, setToggle }) => {
       .catch((err) => {
         isLoading(true);
         setError(err.message);
+        console.log(error);
       });
   }
 
